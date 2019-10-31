512bc520508255504752fa66b9ed9aa13a53ed3b
Luson <993698183@qq.com>
2019-10-31 13:50:19 +0800

    添加date_duration与date_period方法

--- 2/BoostTest/BoostTest/BoostTestDate.h
+++ 2/BoostTest/BoostTest/BoostTestDate.h
@@ -140,4 +140,120 @@ void test_dt_construct()
 	}
 }
 
+//date_duration与date_period
+void test_date_duration()
+{
+	using namespace boost::gregorian;
+
+	// 常见的几种构造方法
+	{
+		date_duration dd(3);
+
+		days dd1(neg_infin);
+		days dd2(pos_infin);
+		days dd3(not_a_date_time);
+		days dd4(max_date_time);
+		days dd5(min_date_time);
+	}
+
+	// 简单运算
+	{
+		date_duration dd1(3);
+		date_duration dd2(5);
+		date_duration dd3 = dd1 + dd2;
+		date_duration dd4 = dd1 - dd2;
+		assert(dd3.days() == 8);
+		assert(dd4.days() == -2);
+	}
+
+	// years, months, days
+	{
+		years ysingle(1);
+		date dy1 = date(2003, 2, 28) + ysingle;
+		assert(dy1 == date(2004, 2, 29));
+		date dy2 = date(2004, 2, 29) + ysingle;
+		assert(dy2 == date(2005, 2, 28));
+
+		months msingle(1);
+		date dm1 = date(2004, 1, 31) + msingle; // => 2004-Feb-29
+		date dm2 = date(2005, 1, 31) + msingle; // => 2005-Feb-28
+		assert(dm1 == date(2004, 2, 29));
+		assert(dm2 == date(2005, 2, 28));
+
+		weeks wsingle(1);
+		date dw1 = date(2005, 1, 1) + wsingle; // => 2005-Jan-08
+		assert(dw1 == date(2005, 1, 8));
+
+		date_duration dd(3);
+		assert(dd.days() == 3);
+	}
+}
+
+void test_date_period()
+{
+	using namespace boost::gregorian;
+
+	// 注意这样的构造函数是左闭右开
+	date_period dp1(date(2010, 11, 29), date(2010, 12, 1));
+	date_period dp2(date(2010, 11, 29), days(2));
+	date_period dp3(dp2);
+
+	// 时间段向后平移3天(dp2 = 12.2-->12.4)
+	dp2.shift(days(3));
+	assert(dp2 == date_period(date(2010, 12, 2), date(2010, 12, 4)));
+
+	// 时间段向前后各扩展1天(dp3 = 11.28-->12.2)
+	dp3.expand(days(1));
+	assert(dp3 == date_period(date(2010, 11, 28), date(2010, 12, 2)));
+
+	// begin, end, last
+	assert(dp1.begin() == date(2010, 11, 29));
+	// 注意end, last的区别。 last是最后一天, end是时间段的右边界（不包含）
+	assert(dp1.end() == date(2010, 12, 1));
+	assert(dp1.last() == date(2010, 11, 30));
+
+	// 时间段的长度
+	// assert(dp1.length() == 2);
+
+	// 一些判断函数(返回值是bool)
+	assert(!dp1.is_null());	// 当起始时间大于结束时间
+	assert(!dp1.contains(date(2010, 12, 1)));	// 也可以包含时间段	
+	bool b1 = dp3.intersects(dp1);	// 除了包含时间点之外, 还可以包含时间段交叉的运算
+	assert(b1 == true);
+	bool b2 = dp3.intersects(dp2);
+	assert(b2 == false);
+	// intersection与intersects不同在于: 它的返回值上date_period
+	date_period dp4(date(2010, 11, 30), date(2010, 12, 3));
+	dp4 = dp4.intersection(dp1);
+	assert(dp4.begin() == date(2010, 11, 30));
+	assert(dp4.end() == date(2010, 12, 1));
+
+	// is_adjacent判断时间段是否相邻. 不能重叠.
+	date_period dp5(date(2010, 12, 1), date(2010, 12, 5));
+	bool b3 = dp1.is_adjacent(dp5);
+	assert(b3);
+
+	// is_before, is_after
+	date d1(2010, 12, 1);
+	bool b4 = dp1.is_before(d1);
+	assert(b4);
+	d1 = d1 - days(1);
+	bool b5 = dp1.is_before(d1);
+	assert(!b5);
+
+	// 时间段合并(merge), 两个没有重合(包括相邻)的时间段, 结果为空.
+	date_period dp6 = dp1.merge(dp3);
+	assert(dp6 == date_period(date(2010, 11, 28), date(2010, 12, 2)));
+
+	// 时间段合并(span)
+	// begin = min(p1.begin, p2.begin)
+	// end = max(p1.end , p2.end)
+	date_period dp7(date(2010, 12, 1), date(2010, 12, 5));
+	date_period dp8(date(2010, 12, 9), date(2010, 12, 31));
+	date_period dp9 = dp7.span(dp8); // 2002-Jan-01/2002-Jan-31
+	assert(dp9 == date_period(date(2010, 12, 1), date(2010, 12, 31)));
+	// 输出: [2010-Dec-01/2010-Dec-30]
+	std::cout << dp9 << std::endl;
+}
+
 #endif // !BOOST_TEST_DATE
