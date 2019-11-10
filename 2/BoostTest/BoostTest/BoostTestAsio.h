96d0fbadb171a714e6d0b561e5172dfe289118cb
Luson <993698183@qq.com>
2019-11-10 09:57:04 +0800

    asion function

--- 2/BoostTest/BoostTest/BoostTestAsio.h
+++ 2/BoostTest/BoostTest/BoostTestAsio.h
@@ -5,6 +5,29 @@
 #include <iostream>
 #include <boost/asio.hpp>
 
+//ASIO,基于操作系统的异步机制，可有效避免多线程编程的诸多副作用
+//定时器的实现
+#include <boost/date_time/posix_time/posix_time.hpp>
+#include <boost/date_time/local_time/local_time.hpp>
+#include <boost/bind.hpp>
+
+void async_timer_callback(asio::deadline_timer* async_timer, int async_count, const system::error_code& err)
+{
+	if (err)
+	{
+		std::cout << "async_timer.async_wait err = " << err.message() << std::endl;
+	} 
+	else
+	{
+		std::cout << "async_timer async_count =" << async_count << "now expires_from_now=" << async_timer->expires_from_now() << std::endl;
+		if (--async_count > 0)
+		{
+			async_timer->expires_at(async_timer->expires_at() + posix_time::milliseconds(500));
+			async_timer->async_wait(boost::bind(async_timer_callback, async_timer, async_count, asio::placeholders::error));
+		}
+	}
+}
+
 void handler(const boost::system::error_code &ec)
 {
 	std::cout << "5s" << std::endl;
