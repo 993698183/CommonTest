dc31227c81c73725c5b926cb087754ee39ecff48
Luson <993698183@qq.com>
2019-11-11 18:01:42 +0800

    easylog

--- 3/EasylogTest/EasylogTest/EasylogTest.cpp
+++ 3/EasylogTest/EasylogTest/EasylogTest.cpp
@@ -2,10 +2,13 @@
 //
 
 #include "stdafx.h"
+#include "easylog/easylogging++.h"
 
+INITIALIZE_EASYLOGGINGPP
 
 int main()
 {
+	//LOG(INFO) << "My first info using default logger";
     return 0;
 }
 
