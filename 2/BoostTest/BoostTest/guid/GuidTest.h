254eedf1881c42a4b829bce96764c3a3a78545ee
Luson <993698183@qq.com>
2019-11-14 17:19:03 +0800

    semaphore and guid

---
+++ 2/BoostTest/BoostTest/guid/GuidTest.h
@@ -0,0 +1,9 @@
+#pragma once
+#ifndef GUID_TEST
+#define GUID_TEST
+
+#include <iostream>
+#include <string>
+
+
+#endif
---
+++ 2/BoostTest/BoostTest/semaphore/SemaphoreTest.h
@@ -0,0 +1,10 @@
+#pragma once
+#ifndef SEMAPHORE_TEST
+#define SEMAPHORE_TEST
+
+//信号量相关操作
+#include <iostream>
+#include <string>
+//#include <semaphore.h>
+
+#endif
