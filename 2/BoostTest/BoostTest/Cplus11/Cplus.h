7070234fb4b006f0b4d344e405f628fc1f5a6b33
Luson <993698183@qq.com>
2020-06-28 13:26:26 +0800

    C11中的for循环

--- 2/BoostTest/BoostTest/Cplus11/Cplus.h
+++ 2/BoostTest/BoostTest/Cplus11/Cplus.h
@@ -386,4 +386,46 @@ void test_lambda_3()
 	for (int it : lbvec)
 		std::cout << it << ' ';
 }
+
+#include <unordered_map>
+void test_for_fun()
+{
+	std::vector<int> vec = { 1, 2, 3, 4 };
+	for (int v : vec)//以值得方式访问
+	{
+		std::cout << v << " ";
+		v = 5;//a中存储的数据不会改变
+	}
+	std::cout << std::endl;
+
+	for (int& v : vec)//通过引用的方式访问vec，可以改变vec的值
+	{
+		v += 100;
+	}
+	for (int v : vec)
+	{
+		std::cout << v << " ";
+	}
+	std::cout << std::endl;
+
+	int b[] = { 21, 22, 23, 24 };
+	for (int v : b)//遍历数组，以值得方式访问
+	{
+		std::cout << v << " ";
+	}
+	std::cout << std::endl;
+
+	std::unordered_map<int, int> map = { {1, 3}, {2, 3}, {3, 3} };
+	for (auto v : map)//遍历map，以值的方式访问
+	{
+		std::cout << v.second << v.first << " ";
+	}
+	std::cout << std::endl;
+
+	for (int v : {41, 42, 43})
+	{
+		std::cout << v << " ";
+	}
+	std::cout << std::endl;
+}
 #endif
